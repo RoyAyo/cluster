5196acd818582336b68d969ca3fa41ae06a7cbf0
RoyAyo <roylayindeay0@gmail.com>
2020-11-16 13:22:36 +0100

    cleared the errors

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const cors = require('cors');
 
 require('dotenv').config();
 
-require('./database/database');
-
 const PORT = process.env.PORT || 5000;
 
 app = express();
@@ -16,21 +14,42 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cors());
 
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+mailchimp.setConfig({
+  apiKey: process.env.API_KEY,
+  server: "YOUR_SERVER_PREFIX"
+});
+
 
 app.post('/register',async (req,res) => {
-    const listId = process.env.AUDIENCE_ID;
-    const subscribingUser = {
-      firstName: req.body.name,
-      email: req.body.email
-    };
-    
-    const response = await mailchimp.lists.addListMember(listId, {
-        email_address: subscribingUser.email,
-        status: "pending",
-        merge_fields: {
-            FNAME: subscribingUser.firstName
-        }
-    });
+    try {
+        const listId = process.env.AUDIENCE_ID;
+        const subscribingUser = {
+        firstName: req.body.name,
+        email: req.body.email
+        };
+        
+        const response = await mailchimp.lists.addListMember(listId, {
+            email_address: subscribingUser.email,
+            status: "pending",
+            merge_fields: {
+                FNAME: subscribingUser.firstName
+            }
+        });
+
+        console.log(response);
+
+        res.json({
+            success: true,
+            msg : 'Successfully added the users'
+        });
+    } catch (e) {
+        res.status(500).json({
+            success: true,
+            msg : 'Successfully added the users'
+        });
+    }
 });
 
 
